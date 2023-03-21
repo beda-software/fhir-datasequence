c8ee30aa93b158b2e668e5c76a2227d5e428b0e7
Nikita Tretyakov <tretyakov.me@gmail.com>
2023-03-21 16:59:10 +0100

    Use OpenAPI to define ingest api request/response schemas

---
+++ fhir_datasequence/api/health_records.py
@@ -0,0 +1,59 @@
+import sqlalchemy
+
+from aiohttp import web
+from sqlalchemy import MetaData, Table, insert
+from aiohttp_apispec import json_schema, docs, response_schema
+from marshmallow import Schema, fields, validate
+
+from fhir_datasequence import config
+
+dbapi_engine = sqlalchemy.create_engine(config.DBAPI_CONN_URL)
+dbapi_metadata = MetaData()
+
+
+class RecordSchema(Schema):
+    sid = fields.Str(required=True)
+    ts = fields.DateTime(format="iso", required=True)
+    code = fields.Str(required=True)
+    duration = fields.Integer(strict=True)
+    energy = fields.Integer(strict=True)
+    start = fields.DateTime(format="iso", required=True)
+    finish = fields.DateTime(format="iso", required=True)
+
+
+class IngestRecordsRequestSchema(Schema):
+    records = fields.List(
+        fields.Nested(RecordSchema), required=True, validate=validate.Length(min=1)
+    )
+
+
+class IngestRecordsResponseSchema(Schema):
+    status = fields.Constant("OK")
+
+
+@docs(summary="Ingest time series data")
+@json_schema(IngestRecordsRequestSchema())
+@response_schema(
+    IngestRecordsResponseSchema(),
+    code=200,
+    description="Time series data has been successfuly persisted",
+)
+async def ingest_health_records(request: web.Request):
+    records_table = Table("records", dbapi_metadata, autoload_with=dbapi_engine)
+    with dbapi_engine.begin() as connection:
+        connection.execute(
+            insert(records_table),
+            [
+                {
+                    "sid": record["sid"],
+                    "ts": record["ts"],
+                    "code": record["code"],
+                    "duration": record.get("duration"),
+                    "energy": record.get("energy"),
+                    "start": record.get("start"),
+                    "finish": record.get("finish"),
+                }
+                for record in request["json"]["records"]
+            ],
+        )
+    return web.json_response({"status": "OK"})
--- fhir_datasequence/main.py
+++ fhir_datasequence/main.py
@@ -1,39 +1,19 @@
-import sqlalchemy
-
 from aiohttp import web
-from sqlalchemy import MetaData, Table, insert
+from aiohttp_apispec import AiohttpApiSpec, validation_middleware
 
-from fhir_datasequence import config
+from fhir_datasequence.api.health_records import ingest_health_records
 
-routes = web.RouteTableDef()
-dbapi_engine = sqlalchemy.create_engine(config.DBAPI_CONN_URL)
-dbapi_metadata = MetaData()
-
-
-@routes.post("/api/v1/records")
-async def ingest_health_records(request: web.Request):
-    payload = await request.json()
-    records_table = Table("records", dbapi_metadata, autoload_with=dbapi_engine)
-    with dbapi_engine.begin() as connection:
-        connection.execute(
-            insert(records_table),
-            [
-                {
-                    "sid": record["sid"],
-                    "ts": record["ts"],
-                    "code": record["code"],
-                    "duration": record.get("duration"),
-                    "energy": record.get("energy"),
-                    "start": record.get("start"),
-                    "finish": record.get("finish"),
-                }
-                for record in payload["records"]
-            ],
-        )
-    return web.json_response({"status": "OK"})
+api_spec: AiohttpApiSpec | None = None
 
 
 async def application() -> web.Application:
-    app = web.Application()
-    app.add_routes(routes)
+    global api_spec
+    app = web.Application(middlewares=[validation_middleware])
+    app.router.add_post("/api/v1/records", ingest_health_records)
+    api_spec = AiohttpApiSpec(
+        app=app,
+        url="/api/docs/openapi.json",
+        title="Time series data storage",
+        version="v1",
+    )
     return app
