13981703d3576688af84c32a770356a27258f5b2
Nikita Tretyakov <tretyakov.me@gmail.com>
2023-03-27 18:01:42 +0200

    Allow authorization with Beda EMR identity tokens only

--- fhir_datasequence/config.py
+++ fhir_datasequence/config.py
@@ -5,4 +5,4 @@ DBAPI_CONN_URL = f"postgresql+psycopg://{environ['PGUSER']}:{environ['PGPASSWORD
 
 APPLE_JWKS_API = "https://appleid.apple.com/auth/keys"
 APPLE_OPENID_ISS_SERVICE = "https://appleid.apple.com"
-APPLE_OPENID_AUD_CLIENT_ID = environ.get("APPLE_OPENID_AUD_CLIENT_ID")
+APPLE_OPENID_AUD_CLIENT_ID = "software.beda.emr"
