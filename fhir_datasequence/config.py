2a8d7c5a25c9c6993ef13e201b20138c52436573
Nikita Tretyakov <tretyakov.me@gmail.com>
2023-03-28 17:07:39 +0200

    Update CORS policy to allow data queries from the web app

--- fhir_datasequence/config.py
+++ fhir_datasequence/config.py
@@ -6,3 +6,5 @@ DBAPI_CONN_URL = f"postgresql+psycopg://{environ['PGUSER']}:{environ['PGPASSWORD
 APPLE_JWKS_API = "https://appleid.apple.com/auth/keys"
 APPLE_OPENID_ISS_SERVICE = "https://appleid.apple.com"
 APPLE_OPENID_AUD_CLIENT_ID = "software.beda.emr"
+
+EMR_WEB_URL = environ.get("EMR_WEB_URL", "https://emr.beda.software/")
--- fhir_datasequence/main.py
+++ fhir_datasequence/main.py
@@ -1,19 +1,32 @@
+import aiohttp_cors
+
 from aiohttp import web
 from aiohttp_apispec import AiohttpApiSpec, validation_middleware
 
+from fhir_datasequence import config
 from fhir_datasequence.api.health_records import (
     ingest_health_records,
     read_health_records,
 )
 
 api_spec: AiohttpApiSpec | None = None
+cors: aiohttp_cors.CorsConfig | None = None
 
 
 async def application() -> web.Application:
-    global api_spec
+    global api_spec, cors
+
     app = web.Application(middlewares=[validation_middleware])
+    cors = aiohttp_cors.setup(app)
     app.router.add_post("/api/v1/records", ingest_health_records)
-    app.router.add_get("/api/v1/records", read_health_records)
+    cors.add(
+        app.router.add_get("/api/v1/records", read_health_records),
+        {
+            config.EMR_WEB_URL: aiohttp_cors.ResourceOptions(
+                allow_headers="*",
+            ),
+        },
+    )
     api_spec = AiohttpApiSpec(
         app=app,
         url="/api/docs/openapi.json",
