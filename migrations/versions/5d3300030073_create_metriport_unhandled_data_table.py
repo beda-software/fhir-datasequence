69e2467292d76761f5f6c5d8e81ccfe27833f0f7
Dmitry Shutov <dmitry.s@beda.software>
2023-08-22 09:52:58 +0700

    Create migration for metriport_unhandled_data table

---
+++ migrations/versions/5d3300030073_create_metriport_unhandled_data_table.py
@@ -0,0 +1,28 @@
+"""create metriport unhandled data table
+
+Revision ID: 5d3300030073
+Revises: e537d7dbd899
+Create Date: 2023-08-22 09:48:51.784647
+
+"""
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy.dialects.postgresql import JSONB
+
+# revision identifiers, used by Alembic.
+revision = "5d3300030073"
+down_revision = "e537d7dbd899"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "metriport_unhandled_data",
+        sa.Column("ts", sa.TIMESTAMP(timezone=True)),
+        sa.Column("data", JSONB),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("metriport_unhandled_data")
